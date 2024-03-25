ed12cf743118f063ac752668dec92d1d41465501
AidenAC <aidenc@umich.edu>
2024-03-25 16:10:38 -0400

    connect ButtonsFragment to Display Fragment

--- app/src/main/java/com/cis436/project2/Buttons.kt
+++ app/src/main/java/com/cis436/project2/Buttons.kt
@@ -1,31 +1,32 @@
 package com.cis436.project2
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cis436.project2.databinding.FragmentButtonsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Buttons.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Buttons : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var activityCallback : Buttons.KeyboardListener
+    private lateinit var binding : FragmentButtonsBinding
+    interface KeyboardListener {
+        fun onButtonClick(character : String)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            activityCallback = context as KeyboardListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(context.toString() + " must implement KeyboardListener")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -33,27 +34,16 @@ class Buttons : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buttons, container, false)
+        binding = FragmentButtonsBinding.inflate(inflater, container, false)
+
+        binding.btZero.setOnClickListener {
+            buttonPressed("0")
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Buttons.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Buttons().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun buttonPressed(character: String) {
+        activityCallback.onButtonClick(character)
     }
 }
--- app/src/main/java/com/cis436/project2/MainActivity.kt
+++ app/src/main/java/com/cis436/project2/MainActivity.kt
@@ -3,9 +3,14 @@ package com.cis436.project2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Buttons.KeyboardListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onButtonClick(character: String) {
+        val display = supportFragmentManager.findFragmentById(R.id.fragmentDisplay) as DisplayFragment
+        display.updateDisplay(character)
     }
 }
